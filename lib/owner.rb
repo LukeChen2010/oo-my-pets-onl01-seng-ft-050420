c3c8ae3805f397bdfaebd71bfd22e2ad9621b943
Luke Chen <luke.chen.2010@gmail.com>
2020-05-10 17:22:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
 class Owner
-  # code goes here
+  attr_accessor :all
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.count
+    return @@all.count
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    return Cat.all.find_all {|x| x.owner == self}
+  end
+  
+  def dogs
+    return Dog.all.find_all {|x| x.owner == self}
+  end
+  
+  def buy_cat(name)
+  end
 end
